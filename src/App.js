365c5aca0a454973031bee1a578bd55b059c10a5
Oleksandra Lukashchuk <83826206+OlexandraLukashchuk@users.noreply.github.com>
2023-05-11 18:01:37 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//import './App.css';
+import LeftMenu from './leftside/left'
+import RightSide from './rightside/rightside'
+function App() {
+  return (
+    <div className="App">
+      <LeftMenu/>
+      <RightSide/>
+    </div>
+  );
+}
+
+export default App;
